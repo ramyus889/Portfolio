3927a1d807da8cc9c9c9467677bf22cd42677403
ramyus889 <ramyus889.com>
2024-06-22 12:45:13 +0300

    fix

--- src/app/ViewServices/components/ViewAboutMe.tsx
+++ src/app/ViewServices/components/ViewAboutMe.tsx
@@ -20,7 +20,7 @@ export default function MPage() {
     },
   ]);
   return (
-    <div className="mx-5 flex place-content-center rounded-xl bg-[#101010] px-[129px] py-[80px] lg:mx-0">
+    <div className="mx-5 flex place-content-center items-center rounded-xl bg-[#101010]  py-[80px] lg:mx-0">
       <div className="flex flex-col items-center gap-2">
         <Image
           src={front}
@@ -30,20 +30,21 @@ export default function MPage() {
           height={80}
         />
         <div className=" text-[22px]">Frontend</div>
-        <div className=" flex  gap-5">
+        <div className=" grid  grid-cols-3 gap-5 ">
           {image.map((item) => (
             <Link
               key={item.img}
               href={item.href}
               target="_blank"
-              className="flex items-center justify-between gap-8 rounded-xl  "
+              className="   gap-8 rounded-xl  "
             >
-              <div className="flex items-center gap-2">
-                <div className="rounded-xl bg-[#282828] px-[12px] py-[12px]">
+              <div className="flex  items-center gap-2 ">
+                <div className=" rounded-xl bg-[#282828] px-[12px] py-[12px]">
                   <Image
                     src={item.img}
-                    className=""
+                    className=" "
                     alt="tech-arsenal"
+                    layout="fixed"
                     width={24}
                     height={24}
                   />
--- src/app/ViewServices/page.tsx
+++ src/app/ViewServices/page.tsx
@@ -9,7 +9,7 @@ import Benefits from "./components/Benefits";
 
 export default function Home() {
   return (
-    <div className="my-[30px]  ">
+    <div className="my-[30px]  overflow-x-hidden">
       <div className="flex place-content-center">
         <div className="flex flex-col gap-5 xxl:flex-row">
           <div className="flex flex-col-reverse gap-5 xxl:flex-row">
